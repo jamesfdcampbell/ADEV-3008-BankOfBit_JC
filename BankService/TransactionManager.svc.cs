9621faa047999d7788d8a40f23ada8052a9263c1
James Campbell <jcampbell3@academic.rrc.ca>
2023-03-15 19:20:32 -0500

    created operation contracts

--- BankService/TransactionManager.svc.cs
+++ BankService/TransactionManager.svc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -13,8 +14,42 @@ namespace BankService
     {
         public void DoWork()
         {
+            
+        }
+
+        public double? BillPayment(int accountId, double amount, string notes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double? CalculateInterest(int accountId, string notes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double? Deposit(int accountId, double amount, string notes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double? Transfer(int fromAccountId, int toAccountId, double amount, string notes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double? Withdrawal(int accountId, double amount, string notes)
+        {
+            throw new NotImplementedException();
         }
 
         private double? UpdateBalance(int accountId, double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateTransaction(int accountId, double amount, int transactionTypeId, string notes)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
